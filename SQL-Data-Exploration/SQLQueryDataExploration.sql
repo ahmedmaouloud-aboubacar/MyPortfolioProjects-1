acbb9be3422fed70992f473615bf2f4735cc57ce
Aboubcar AhmedMaouloud <64739623+ahmedmaouloud-aboubacar@users.noreply.github.com>
2024-02-11 03:11:26 +0100

    Update SQLQueryDataExploration.sql

--- SQL-Data-Exploration/SQLQueryDataExploration.sql
+++ SQL-Data-Exploration/SQLQueryDataExploration.sql
@@ -1,11 +1,20 @@
---Select all Data From CovidDeaths and CovidVaccinations  tables
+----------------------------------------------
+--------  Retrieving Data from Tables  -------
+----------------------------------------------
+--Select all data from the CovidDeaths table
 SELECT * FROM PortfolioProject..CovidDeaths$ 
 WHERE continent IS NOT NULL
 ORDER BY 1,2
+
+--Select all data from the CovidVaccinations table
 SELECT * FROM PortfolioProject..CovidVaccinations$ 
 WHERE continent IS NOT NULL
 ORDER BY 1,2
---Select Data that we are going to use
+
+	---------------------------------------------------
+	-------- Analyzing COVID Cases and Deaths ---------
+	---------------------------------------------------
+--Select Data that we are going to use from the CovidDeaths table
 SELECT location,Date,total_cases,new_cases,total_deaths,population 
 FROM PortfolioProject..CovidDeaths$
 ORDER BY 1,6
@@ -15,7 +24,7 @@ SELECT location,Date, total_cases,total_deaths,(convert(float ,total_deaths)/ co
 FROM PortfolioProject..CovidDeaths$
 WHERE total_deaths >= total_cases  AND location like '%auritan%'   
 ORDER BY 1,2 
--- Shows what percentage of population got Covid IN Mauritania
+-- Shows what percentage of population got Covid
 SELECT location,Date, total_cases, population,(total_cases/population)*100 AS PercentPopulationInfected 
 FROM PortfolioProject..CovidDeaths$ 
 WHERE total_cases IS NOT NULL-- AND location like '%aurita%'  
@@ -24,12 +33,12 @@ ORDER BY 1,2
 -- Looking at The countries With Highest Infection Rate Compared To Population
 SELECT location, MAX(total_cases) as HighestIfectionCount,Max((total_cases/population)*100) as PercentPopulationInfected
 FROM PortfolioProject..CovidDeaths$ 
-WHERE total_cases IS NOT NULL AND population IS NOT NULL AND date<'2021-06-15'
+WHERE total_cases IS NOT NULL AND population IS NOT NULL --AND date<'2021-06-15'
 Group By location, population
 ORDER BY PercentPopulationInfected DESC
 
 -- Looking at The countries With Highest Death Rate Compared To Population
-SELECT location, MAX(cast(total_deaths as int)) as TotalDeathCount--,(convert(float ,total_deaths)/ convert(float ,total_cases) )*100 as DeathPercentage
+SELECT location, MAX(cast(total_deaths as int)) as TotalDeathCount
 FROM PortfolioProject..CovidDeaths$ 
 WHERE continent IS NOT NULL
 Group By location, population
@@ -49,17 +58,21 @@ WHERE continent IS NOT NULL
 Group By continent
 ORDER BY TotalDeathCount DESC
 
---GLOBAL Number
+--GLOBAL Numbers
 SELECT date,SUM(new_cases) AS TotalPopulationInfected
 		   ,SUM(cast(new_deaths as int)) as TotalPopulationDeath
 		   ,SUM(cast(new_deaths as int))/ SUM(new_cases)*100 AS DeathPercentage
 FROM PortfolioProject..CovidDeaths$
-WHERE continent IS NOT NULL AND new_cases !=0 
---Group By date
+WHERE continent IS NOT NULL AND new_cases !=0
 ORDER BY 1,2 DESC
 
+
+		----------------------------------------
+		-------- Analyzing Vaccinations --------
+		----------------------------------------
+	
 --Looking At Total Population Vs Vaccinations 
---USE CTE
+-- 1) Using Common Table Expression (CTE)
 WITH PopvsVac (Continent,location,Date,Population,new_vaccinations,RollingPeopleVacinated)
 AS
 (SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
@@ -69,12 +82,10 @@ FROM PortfolioProject..CovidDeaths$ dea
 JOIN PortfolioProject..CovidVaccinations$ vac
 ON dea.location=vac.location AND dea.date=vac.date
 WHERE dea.continent IS NOT NULL
---ORDER BY 2,3
 )
 SELECT * , RollingPeopleVacinated/Population*100 FROM PopvsVac
---USE CTE : WITH POPvsVAC (Coulmn1, Coulmn2,...) AS SELECT QUERY
 
---TEMP TABLE
+-- 2) Using Temporary Table
 DROP TABLE IF EXISTS #PourcentPopulationVaccinated
 CREATE TABLE #PourcentPopulationVaccinated
 (
@@ -84,7 +95,7 @@ Date datetime,
 Population numeric,
 new_vaccinations numeric,
 RollingPeopleVacinated numeric
-)
+);
 
 INSERT INTO #PourcentPopulationVaccinated
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
@@ -96,7 +107,7 @@ ON dea.location=vac.location AND dea.date=vac.date
 WHERE dea.continent IS NOT NULL
  SELECT *  FROM #PourcentPopulationVaccinated
 
---Creating View to store data for later visualozation
+-- Creating a View to store data for later visualization
 DROP VIEW IF EXISTS PourcentPopulationVaccinated
 CREATE VIEW PourcentPopulationVaccinated AS
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
@@ -106,7 +117,8 @@ FROM PortfolioProject..CovidDeaths$ dea
 JOIN PortfolioProject..CovidVaccinations$ vac
 ON dea.location=vac.location AND dea.date=vac.date
 WHERE dea.continent IS NOT NULL
- SELECT * FROM PourcentPopulationVaccinated
+
+SELECT * FROM PourcentPopulationVaccinated
 
 
  
